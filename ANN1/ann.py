eae04aee3000a309b839d1a730bf4d4c47f7d65e
abhishek-karmakar <abhishek.karmakar.micromaxinfo.com>
2017-11-14 16:49:43 +0530

    Encoding the variables to convert them from Text to Numbers.

--- ANN1/ann.py
+++ ANN1/ann.py
@@ -6,9 +6,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
